1a4a98ae3aeb26ec6c7220ab07fcf765335b130f
LMGSLeslie <lesmagasa@hotmail.com>
2019-04-12 16:52:56 -0500

    :)

--- src/Dashboards/Author.js
+++ src/Dashboards/Author.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './Author.css';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import { Legend } from '@devexpress/dx-react-chart-material-ui';
+// import { PieSeries } from '@devexpress/dx-react-chart-bootstrap4';
+import { ValueScale, Animation } from '@devexpress/dx-react-chart';
+import { List, Create, Edit, SimpleForm, DisabledInput, TextInput, DateInput, LongTextInput, ReferenceManyField, Datagrid, TextField, DateField, EditButton } from 'react-admin';
 
+import { Chart, ArgumentAxis, ValueAxis, LineSeries, PieSeries, BarSeries } from "@devexpress/dx-react-chart-material-ui";
 
+var cardStyle = {
+    width: '100%',
+    height: '100%',
+    marginBottom: '5%'
+  
+  }
+
+  
 const areas = [
         {'id': '1', 'nombre_area':'Computación'},
         {'id': '2', 'nombre_area':'Matemáticas'},
@@ -45,7 +62,52 @@ const Author = () =>(
                 
             </div>
         </form>
+            <Grid container spacing={8}>
+    <Grid item xs={6}>
+    <Card style={cardStyle}
+    >
+        <CardHeader title="Publicaciones por área" />
+        <CardContent>
+
+        <Chart
+            data={[
+            { argument: "Financiera", value: 10 },
+            { argument: "Agricultura", value: 20 },
+            { argument: "Medicina", value: 30 },
+            { argument: "Arte", value: 10 },
+            { argument: "Química", value: 20 },
+            { argument: "Ciencia de la computación", value: 30 },
+            { argument: "Ciencias Biologicas", value: 10 },
+            { argument: "Economía", value: 20 },
+            { argument: "Ley y Política", value: 30 }
+            ]}
+        >
+            <ValueScale name="sale" />
+            <ValueScale name="total" />
+            <PieSeries name="publicaciones" valueField="value" argumentField="argument" />
+
+            <Animation />
+            <Legend />
+        </Chart>
+
+
+        <SimpleForm>
+            <TextInput source="Sucursal" />
+            <TextInput source="Nombre" />
+            <TextInput source="Departamento" />
+            <TextInput source="Puesto" />
+            <TextInput source="Correo" />
+            <TextInput source="Contraseña" />
+            <TextInput source="Rol" />
+        </SimpleForm>
+        </CardContent>
+
+    </Card>
+    </Grid>
+    </Grid>
     </div>
+
+
 );
 
 export default Author;
